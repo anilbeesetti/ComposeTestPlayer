24b82d93074f931b578e765157ccccb5fa29d58b
anilbeesetti <anilbeesetti10@gmail.com>
2022-10-22 11:12:23 +0530

    navigation addded to settingroup

--- feature/media/src/main/java/com/arcticoss/nextplayer/feature/media/settings/SettingGroup.kt
+++ feature/media/src/main/java/com/arcticoss/nextplayer/feature/media/settings/SettingGroup.kt
@@ -2,6 +2,7 @@ package com.arcticoss.nextplayer.feature.media.settings
 
 import androidx.annotation.StringRes
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Aod
 import androidx.compose.material.icons.rounded.Info
 import androidx.compose.material.icons.rounded.List
 import androidx.compose.material.icons.rounded.PlayArrow
@@ -11,23 +12,33 @@ import com.arcticoss.nextplayer.feature.media.R
 data class SettingGroup(
     @StringRes val title: Int,
     @StringRes val description: Int,
-    val icon: ImageVector
+    val icon: ImageVector,
+    val navigateTo: SettingsNavigateTo
 )
 
 val settingGroupList: List<SettingGroup> = listOf(
     SettingGroup(
-        title = R.string.list,
-        description = R.string.list_description,
-        icon = Icons.Rounded.List
+        title = R.string.interface_name,
+        description = R.string.interface_description,
+        icon = Icons.Rounded.Aod,
+        navigateTo = SettingsNavigateTo.Interface
     ),
     SettingGroup(
         title = R.string.player,
         description = R.string.player_description,
-        icon = Icons.Rounded.PlayArrow
+        icon = Icons.Rounded.PlayArrow,
+        navigateTo = SettingsNavigateTo.Player
     ),
     SettingGroup(
         title = R.string.about,
         description = R.string.about_description,
-        icon = Icons.Rounded.Info
+        icon = Icons.Rounded.Info,
+        navigateTo = SettingsNavigateTo.About
     )
 )
+
+
+
+enum class SettingsNavigateTo {
+    Interface, Player, About
+}
