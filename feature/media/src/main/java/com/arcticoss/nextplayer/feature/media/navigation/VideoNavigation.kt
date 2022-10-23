91110d0af4a499295c9a8e1afdba00add717ae5f
anilbeesetti <anilbeesetti10@gmail.com>
2022-10-23 10:20:24 +0530

    defined navigation to VideosScreen

---
+++ feature/media/src/main/java/com/arcticoss/nextplayer/feature/media/navigation/VideoNavigation.kt
@@ -0,0 +1,29 @@
+package com.arcticoss.nextplayer.feature.media.navigation
+
+import androidx.navigation.*
+import androidx.navigation.compose.composable
+import com.arcticoss.nextplayer.feature.media.screens.video.VideosScreen
+
+const val videosScreenNavigationRoute = "videos_screen_route"
+internal const val folderIdArg = "folderId"
+
+fun NavController.navigateToVideosScreen(folderId: Long, navOptions: NavOptions? = null) {
+    this.navigate("$videosScreenNavigationRoute/$folderId", navOptions)
+}
+
+fun NavGraphBuilder.videoScreen(
+    onBackClick: () -> Unit,
+    onMediaItemClick: (path: String) -> Unit
+) {
+    composable(
+        route = "$videosScreenNavigationRoute/{$folderIdArg}",
+        arguments = listOf(
+            navArgument(folderIdArg) { type = NavType.LongType}
+        )
+    ) {
+        VideosScreen(
+            onNavItemClick = onBackClick,
+            onMediaItemClick = onMediaItemClick
+        )
+    }
+}
