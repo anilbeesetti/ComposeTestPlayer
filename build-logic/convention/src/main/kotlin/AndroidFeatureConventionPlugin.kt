6cd6074506717495340ab43aadf6f64374f28a07
anilbeesetti <anilbeesetti10@gmail.com>
2022-11-19 14:24:19 +0530

    added convention plugins

---
+++ build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -0,0 +1,20 @@
+import com.android.build.api.dsl.LibraryExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+
+class AndroidFeatureConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            pluginManager.apply {
+                apply("nextplayer.android.library")
+                apply("nextplayer.android.hilt")
+            }
+            extensions.configure<LibraryExtension> {
+                defaultConfig {
+                    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+                }
+            }
+        }
+    }
+}
