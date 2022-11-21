4a45ecdc53e8dd326119395e5af40d42c8371a73
anilbeesetti <anilbeesetti10@gmail.com>
2022-11-21 11:54:48 +0530

    video rotation based on video dimensions

--- feature/player/src/main/java/com/arcticoss/nextplayer/feature/player/compose/VideoScreen.kt
+++ feature/player/src/main/java/com/arcticoss/nextplayer/feature/player/compose/VideoScreen.kt
@@ -1,6 +1,7 @@
 package com.arcticoss.nextplayer.feature.player.compose
 
 import android.annotation.SuppressLint
+import android.content.pm.ActivityInfo
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -11,6 +12,7 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.arcticoss.nextplayer.feature.player.PlayerViewModel
 import com.arcticoss.nextplayer.feature.player.presentation.composables.SurfaceType
 import com.arcticoss.nextplayer.feature.player.presentation.composables.VideoSurface
+import com.arcticoss.nextplayer.feature.player.presentation.isPortrait
 import com.arcticoss.nextplayer.feature.player.presentation.rememberMediaState
 import com.arcticoss.nextplayer.feature.player.utils.findActivity
 
@@ -28,6 +30,16 @@ fun VideoScreen(
     val exoplayerState by viewModel.exoplayerStateStateFlow.collectAsStateWithLifecycle()
     val context = LocalContext.current
     val activity = context.findActivity()
+
+    LaunchedEffect(key1 = mediaState.playerState?.videoFormat) {
+        mediaState.playerState?.videoFormat?.let {
+            if (it.isPortrait) {
+                activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT
+            } else {
+                activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE
+            }
+        }
+    }
 
     Box(modifier = Modifier.fillMaxSize()) {
         DisposableEffect(
