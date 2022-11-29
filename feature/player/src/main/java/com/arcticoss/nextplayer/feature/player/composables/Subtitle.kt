24e8e4226c932e61519c1d361b29db86682fddf3
Anil404 <66936649+anilbeesetti@users.noreply.github.com>
2022-11-29 12:46:45 +0530

    Improved subtitle text and added gestures (#55)

--- feature/player/src/main/java/com/arcticoss/nextplayer/feature/player/composables/Subtitle.kt
+++ feature/player/src/main/java/com/arcticoss/nextplayer/feature/player/composables/Subtitle.kt
@@ -1,28 +1,78 @@
 package com.arcticoss.nextplayer.feature.player.composables
 
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.offset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.sp
 import com.google.android.exoplayer2.text.CueGroup
+import kotlin.math.roundToInt
 
 @Composable
 fun Subtitle(
     cueGroup: CueGroup,
     modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        cueGroup.cues.forEach {
-            Text(
-                text = it.text.toString(),
-                textAlign = TextAlign.Center
+    var offsetY by remember { mutableStateOf(-20f) }
+    var scale by remember { mutableStateOf(1f) }
+    val state = rememberTransformableState { zoomChange, _, _ ->
+        scale *= zoomChange
+    }
+
+    Box(
+        modifier = modifier
+            .offset { IntOffset(0, offsetY.roundToInt()) }
+            .draggable(
+                orientation = Orientation.Vertical,
+                state = rememberDraggableState { delta ->
+                    if (offsetY + delta < 0) {
+                        offsetY += delta
+                    }
+                }
             )
-            it.textAlignment
+            .graphicsLayer {
+                scaleX = scale
+                scaleY = scale
+            }
+            .transformable(state = state),
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            cueGroup.cues.forEach {
+                Text(
+                    text = it.text.toString(),
+                    textAlign = TextAlign.Center,
+                    style = TextStyle(
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 24.sp,
+                        shadow = Shadow(
+                            color = Color.Black,
+                            offset = Offset(5.0f, 5.0f)
+                        )
+                    )
+                )
+            }
         }
     }
 }
